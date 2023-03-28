c2b000f9acc9530b20a49a125aafeaf09022a130
Zoli0217 <113987205+Zoli0217@users.noreply.github.com>
2023-03-28 11:10:24 +0200

    *

--- memory.js
+++ memory.js
@@ -1,5 +1,6 @@
 let playground = document.getElementById("playground")
-
+let time = document.getElementById("time")
+let moves = document.getElementById("moves")
 let cards = [
     "A", "A",
     "B", "B",
@@ -16,6 +17,45 @@ cards.sort( (a,b) => {
     return rnd
 } )
 
+function gameIsOver(){
+    if(document.getElementsByClassName("good").length == cards.length){
+        return true
+    }
+    return false
+}
+
+
+function startTimer(){
+    let sec = 0
+    let min = 0
+    let timerInterval = setInterval(()=>{
+        sec++
+
+        if(sec>= 60){
+            min++
+            sec-=60
+        }
+
+
+        time.innerText = min + ':' + sec
+
+        if(gameIsOver()){
+            clearInterval(timerInterval)
+        }
+    },1000)
+}
+
+startTimer()
+
+
+
+
+function addMove(){
+    let num = moves.innerText * 1
+    num++
+    moves.innerText = num
+}
+
 function test(){
     let pickeds = document.getElementsByClassName("picked")
     pickeds = Array.from(pickeds)
@@ -23,14 +63,18 @@ function test(){
     if(pickeds.length < 2){
         return 0;
     }else{
-        if(pickeds[0].innerHTML === pickeds[1].innerHTML){
-            alert("egyenlő!")
-        }else{
-            pickeds[0].innerHTML = ""
-            pickeds[1].innerHTML = ""
-        }
-        pickeds[0].classList.remove("picked")
-        pickeds[1].classList.remove("picked")
+        setTimeout(()=>{
+            if(pickeds[0].innerHTML === pickeds[1].innerHTML){
+                pickeds[0].classList.add("good")
+                pickeds[1].classList.add("good")
+            }else{
+                pickeds[0].innerHTML = ""
+                pickeds[1].innerHTML = ""
+            }
+            pickeds[0].classList.remove("picked")
+            pickeds[1].classList.remove("picked")
+        },500)
+        
     }
 
     
@@ -40,8 +84,19 @@ for(let i=0; i<16; i++){
     let card = document.createElement("div")
     card.classList.add("card")
     card.onclick = () => {
+        if(document.getElementsByClassName("picked").length >= 2){
+            return
+        }
+
+        if(card.classList.contains("picked") ||
+        card.classList.contains("good")){
+            return
+        } 
+
+
         card.innerHTML = cards[i]
         card.classList.add("picked")
+        addMove()
         test()
     }
     playground.appendChild(card)
